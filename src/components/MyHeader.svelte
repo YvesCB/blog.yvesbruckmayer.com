8c1e3f4b88d3f2f7fb4557589485432a8d0b3cb9
YvesCB <y.bruckmayer@gmail.com>
2023-10-27 13:48:56 +0200

    preparing basic structure

---
+++ src/components/MyHeader.svelte
@@ -0,0 +1,45 @@
+<header>
+	<div class="hero">
+		<h1>BIG TITLE</h1>
+	</div>
+	<nav>
+		<ul>
+			<li><a href="/">HOME</a></li>
+			<li><a href="/blog">BLOG</a></li>
+			<li><a href="/friends">FRIENDS</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		width: 100vw;
+	}
+
+	.hero {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	nav {
+		max-width: 80rem;
+		margin: auto;
+	}
+
+	ul {
+		display: flex;
+		justify-content: space-around;
+	}
+
+	li {
+		list-style: none;
+	}
+
+	a {
+		text-decoration: none;
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import MyHeader from "../components/MyHeader.svelte";
+</script>
+
+<svelte:head>
+	<title>Playground</title>
+	<meta name="robots" content="noindex nofollow" />
+	<html lang="en" />
+</svelte:head>
+
+<MyHeader />
+
+<slot />
+
+<style>
+	:global(*),
+	:global(*::after) {
+		box-sizing: border-box;
+	}
+</style>
